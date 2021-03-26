6750b40513ee6df0578e9dae1420d8792953b1c6
nagarjuna <nagarjuna.reddy907@gmail.com>
2021-03-26 10:44:12 -0400

    Search issue fixed

--- frontend/src/app/shared/components/table/ng-filter.pipe.ts
+++ frontend/src/app/shared/components/table/ng-filter.pipe.ts
@@ -5,14 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NgFilterPipe implements PipeTransform {
   transform(values: any[], filter: string, data: any[]): any {
+    values = [];
+    values = data;
     if (!values || !values.length) return [];
     if (!filter) return values;
 
     filter = filter.toUpperCase();
     if (filter && Array.isArray(values)) {
       const keys = Object.keys(values[0]);
-      values = [];
-      values = data;
+
       return values.filter(
         (v) =>
           v &&
