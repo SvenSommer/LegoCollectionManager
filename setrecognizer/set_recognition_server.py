2dd50aaa9190a7bf160e1da75f3d466bd41ea123
Алексей Коваленко <alexey@MacBook-Pro-Aleksej.local>
2021-04-20 22:08:28 +0300

    Add server api template

---
+++ setrecognizer/set_recognition_server.py
@@ -0,0 +1,88 @@
+from argparse import ArgumentParser, Namespace
+from flask import Flask, jsonify, abort, request, Response
+import io
+import logging
+import numpy as np
+import requests
+from PIL import Image
+from functools import reduce
+
+
+def args_parse() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument('--ip', required=False, type=str, default='0.0.0.0')
+    parser.add_argument('--port', required=False, type=int, default=5000)
+    return parser.parse_args()
+
+
+app = Flask(__name__)
+app_log = logging.getLogger('werkzeug')
+app_log.setLevel(logging.ERROR)
+
+
+def solution_inference(img: np.ndarray) -> dict:
+    """
+    Pipeline inference
+    Args:
+        img: image in uint8 RGB HWC format
+
+    Returns:
+        List with detections in [box, set number] format
+    """
+    if img is None:
+        abort(409)
+
+    # Now this method returns random values
+    res = {
+        'detections': []
+    }
+
+    for i in range(np.random.randint(0, 10)):
+        x = np.random.randint(0, img.shape[1])
+        y = np.random.randint(0, img.shape[0])
+        w = np.random.randint(img.shape[1] // 10, img.shape[1] - x - 1)
+        h = np.random.randint(img.shape[0] // 10, img.shape[0] - y - 1)
+
+        number = reduce(
+            lambda s1, s2: s1 + s2,
+            [str(k) for k in np.random.randint(0, 10, size=5)]
+        )
+
+        res['detections'].append(
+            {
+                'box': [
+                    x / img.shape[1],
+                    y / img.shape[0],
+                    w / img.shape[1],
+                    h / img.shape[0],
+                    number
+                ],
+                'number': number
+            }
+        )
+
+    return res
+
+
+@app.route('/api/inference/', methods=['GET'])
+def solution_inference_by_url():
+    image_url_str = request.args.get('image')
+
+    image = None
+    try:
+        response = requests.get(image_url_str)
+        image = np.array(
+            Image.open(io.BytesIO(response.content)).convert('RGB')
+        )
+    except Exception as e:
+        logging.error(
+            'From method solution_inference_by_url(image_url), {}'.format(e)
+        )
+        abort(408)
+
+    return jsonify(solution_inference(image))
+
+
+if __name__ == '__main__':
+    args = args_parse()
+    app.run(host=args.ip, debug=False, port=args.port)
